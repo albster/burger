ca3e205a119168881eb351ea5b55d51235636f02
albster <albster@mac.com>
2019-05-02 11:00:06 -0400

    added assets folder

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,30 @@
 var orm = require(".../config/orm");
+
+
+
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burger_Controllers.js).
+module.exports = burger;
